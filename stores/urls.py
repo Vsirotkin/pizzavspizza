83e91e97aede7c39e01042702df6710571ad279e
Vsirotkin <vsirotkin15@gmail.com>
2022-04-29 19:08:37 +0300

    added delete func > views, urls

--- stores/urls.py
+++ stores/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('detail/<int:id>/', PizzeriaRetrieveAPIView.as_view(), name='pizzeria_detail'),
     path('create/', PizzeriaCreateAPIView.as_view(), name='pizzeria_create'),
     path('update/<int:id>/', PizzeriaRetrieveUpdateAPIView.as_view(), name='pizzeria_update'),
+    path('delete/<int:id>/', PizzeriaDestroyAPIView.as_view(), name='pizzeria_delete'),
 ]
