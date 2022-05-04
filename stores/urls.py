984af8b721a683519b7ddb259cf28288a64bb598
Vsirotkin <vsirotkin15@gmail.com>
2022-05-04 11:53:11 +0300

    urls

--- stores/urls.py
+++ stores/urls.py
@@ -10,3 +10,4 @@ urlpatterns = [
     path('update/<int:id>/', PizzeriaRetrieveUpdateAPIView.as_view(), name='pizzeria_update'),
     path('delete/<int:id>/', PizzeriaDestroyAPIView.as_view(), name='pizzeria_delete'),
 ]
+
